a85ae034fe93d078a9685e97c40332d7a2bfb37c
colficol-jpg <colficol@gmail.com>
2025-10-20 01:17:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ if ('serviceWorker' in navigator) {
     .then(() => console.log('Service Worker registrado'))
     .catch(() => console.log('Error al registrar Service Worker'));
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(() => console.log('Service Worker registrado'))
+      .catch((err) => console.log('Error registrando Service Worker:', err));
+  });
+}
