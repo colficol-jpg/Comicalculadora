2fdd0db35b34f02612501ff0303e82a1c20bec49
colficol-jpg <colficol@gmail.com>
2025-10-20 01:17:25 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,10 @@
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
