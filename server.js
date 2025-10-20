797722ad7525aec5354f5c4b440bf3f2b835aef1
colficol-jpg <colficol@gmail.com>
2025-10-19 22:49:30 -0500

    Refactor server routes and update log message

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Servidor funcionando en puerto ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Servidor corriendo en el puerto ${PORT}`));
